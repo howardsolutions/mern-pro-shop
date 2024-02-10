82f71d4a2df173b9f9ddf67a08eb4a8d83181c91
thinhphungdev <phungthinh0946519356@gmail.com>
2024-02-10 17:24:26 +0700

    product schema

---
+++ server/models/productModel.js
@@ -0,0 +1,59 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User', // specify which collection this is comming from
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    ratings: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true, // automatically generated the createdAt field
+  }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
