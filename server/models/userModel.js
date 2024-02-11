44d7d143928156ff3ca2c783ea1fc2d96c60d310
thinhphungdev <phungthinh0946519356@gmail.com>
2024-02-11 16:48:44 +0700

    feat: order model schema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,24 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
