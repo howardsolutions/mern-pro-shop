1b186a2aea7f59481f5cde80ca382b1a776ae017
thinhphungdev <phungthinh0946519356@gmail.com>
2024-02-17 18:14:16 +0700

    feat: init redux

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -10,6 +10,10 @@ import {
 import HomeScreenPage from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
+// Redux
+import store from './store';
+import { Provider } from 'react-redux';
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
@@ -23,6 +27,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provide store={store}>
+      <RouterProvider router={router} />
+    </Provide>
   </React.StrictMode>
 );
