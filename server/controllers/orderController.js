7af0952702594fb0d04be3362b240c1d13eb2372
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-04 10:17:12 +0700

    update order status to delivered on the server

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -94,12 +94,12 @@ const updateOrderStatusToPaid = asyncHanler(async (req, res) => {
     id: req.body.id,
     status: req.body.status,
     update_time: req.body.update_time,
-    email_address: req.body.payer.email_address,
+    email_address: req.body?.payer?.email_address,
   };
 
-  const udpatedOrder = await order.save();
+  const updatedOrder = await order.save();
 
-  res.status(200).json(udpatedOrder);
+  res.status(200).json(updatedOrder);
 });
 
 /**
@@ -108,7 +108,19 @@ const updateOrderStatusToPaid = asyncHanler(async (req, res) => {
  * @access PRIVATE / admin
  */
 const updateOrderStatusToDelivered = asyncHanler(async (req, res) => {
-  res.send('update order to delivered');
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+
+  order.isDelivered = true;
+  order.deliveredAt = Date.now();
+
+  const updatedOrder = await order.save();
+
+  res.status(200).json(updatedOrder);
 });
 
 /**
@@ -117,7 +129,8 @@ const updateOrderStatusToDelivered = asyncHanler(async (req, res) => {
  * @access PRIVATE / admin
  */
 const getOrders = asyncHanler(async (req, res) => {
-  res.send('admin get all orders');
+  const orders = await Order.find({}).populate('user', 'id name');
+  res.json(orders);
 });
 
 export {
