f6709f8cd92f9f061d1bf0f7fda2deeb602fb4ea
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-04 11:17:57 +0700

    init Product list screen

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -23,7 +23,8 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import AdminRoute from './components/AdminRoute';
-import OrderListScreen from './components/OrderListScreen';
+import OrderListScreen from './screens/admin/OrderListScreen';
+import ProductListScreen from './screens/admin/ProductListScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -45,6 +46,7 @@ const router = createBrowserRouter(
 
       <Route path='' element={<AdminRoute />}>
         <Route path='/admin/orderlist' element={<OrderListScreen />} />
+        <Route path='/admin/productlist' element={<ProductListScreen />} />
       </Route>
     </Route>
   )
