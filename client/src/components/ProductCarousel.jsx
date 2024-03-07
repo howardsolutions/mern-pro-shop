03c43a6312c19ca921655fb9428bf8395a0e94f1
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-07 11:54:58 +0700

    feat: add product carousel to homescreen

--- client/src/components/ProductCarousel.jsx
+++ client/src/components/ProductCarousel.jsx
@@ -19,7 +19,7 @@ const ProductCarousel = () => {
 
   return (
     <Carousel pause='hover' className='bg-primary mb-4'>
-      {products.map((product) => (
+      {products?.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -3,8 +3,9 @@ import { Row, Col } from 'react-bootstrap';
 import { Product } from '../components';
 import { useProducts } from '../hooks/index';
 import Loader from '../components/Loader';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Paginate from '../components/Paginate';
+import ProductCarousel from '../components/ProductCarousel';
 
 function HomeScreenPage() {
   const { pageNumber, keywords } = useParams();
@@ -14,21 +15,30 @@ function HomeScreenPage() {
 
   return (
     <div>
-      <h1>Latest Product</h1>
-      <Row>
-        {data &&
-          data.products &&
-          data.products.map((product) => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          ))}
-      </Row>
-      <Paginate
-        pages={data?.pages}
-        currentPage={data?.page}
-        keywords={keywords ? keywords : ''}
-      />
+      {!keywords ? (
+        <ProductCarousel />
+      ) : (
+        <Link to='/' className='btn btn-light mb-4'>
+          Go Back
+        </Link>
+      )}
+      <>
+        <h1>Latest Product</h1>
+        <Row>
+          {data &&
+            data.products &&
+            data.products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+        </Row>
+        <Paginate
+          pages={data?.pages}
+          currentPage={data?.page}
+          keywords={keywords ? keywords : ''}
+        />
+      </>
     </div>
   );
 }
