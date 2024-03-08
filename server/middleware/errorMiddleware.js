65771448f1cd7157cb01cbc602514680358751c0
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-08 15:19:50 +0700

    feat: add checkobjID mongo Db middleware

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -9,12 +9,6 @@ const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 
-  // check the mongoose bad ObjectId
-  if (err.name === 'CastError' && err.kind === 'ObjectId') {
-    message = `Resource not found`;
-    statusCode = 404;
-  }
-
   res.status(statusCode).json({
     message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -10,16 +10,18 @@ import {
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
+import checkObjectId from '../middleware/checkObjectId.js';
+
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.get('/top', getTopProducts);
 router
   .route('/:id')
-  .get(getProductById)
-  .delete(protect, admin, deleteProductById)
-  .put(protect, admin, updateProduct);
+  .get(checkObjectId, getProductById)
+  .delete(protect, admin, checkObjectId, deleteProductById)
+  .put(protect, admin, checkObjectId, updateProduct);
 
-router.route('/:id/reviews').post(protect, createProductReview);
+router.route('/:id/reviews').post(protect, checkObjectId, createProductReview);
 
 export default router;
