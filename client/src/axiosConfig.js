371e848b4b9dfccd0c3eb6277fa701467e59099b
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-13 15:07:40 +0700

    fix: axios config

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 
+let BACKEND_BASE_URL;
+
+if (process.env.NODE_ENV !== 'production') {
+  BACKEND_BASE_URL = 'http://localhost:4000';
+} else {
+  BACKEND_BASE_URL = 'https://mern-proshop-7sp5.onrender.com';
+}
+
 export const axiosInstance = axios.create({
-  baseURL: process.env.BASE_URL || 'http://localhost:4000',
+  baseURL: BACKEND_BASE_URL,
   withCredentials: true,
 });
